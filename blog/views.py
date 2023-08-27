a50434e76dd54b1d04845abb87626d4bf6f4315b
andri <andriyparayko2017@gmail.com>
2023-08-26 19:29:32 -0700

    vid126

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from datetime import date
+from .models import Post
+
+all_posts = [
+
+]
 
 def get_date(post):
   return post['date']
 
 def starting_page(request):
-  sorted_posts = sorted(all_posts, key=get_date)
-  latest_posts = sorted_posts[-3:]
+  latest_posts = Post.objects.all().order_by("-date")[:3]
   return render(request, "blog/index.html", {
-    "posts":latest_posts
+    "posts": latest_posts
   })
 
 def posts(request):
