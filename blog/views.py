72792303bea8eb8a2e19618c54a1d6f531f465fd
andri <andriyparayko2017@gmail.com>
2023-08-26 19:34:24 -0700

    vid127

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,5 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-
-all_posts = [
-
-]
-
-def get_date(post):
-  return post['date']
 
 def starting_page(request):
   latest_posts = Post.objects.all().order_by("-date")[:3]
@@ -16,12 +8,13 @@ def starting_page(request):
   })
 
 def posts(request):
+  all_posts = Post.objects.all().order_by("-date")
   return render(request, "blog/all-posts.html", {
     "all_posts": all_posts
   })
 
 def post_detail(request, slug):
-  identefied_post = next(post for post in all_posts if post['slug'] == slug)
+  identefied_post = get_object_or_404(Post, slug=slug)
   return render(request, "blog/post-detail.html", {
     "post": identefied_post
   })
